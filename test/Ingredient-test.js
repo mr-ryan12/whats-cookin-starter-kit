10754bdb893142b82ba68582c4c4602522fae9c0
Ryan McBride <mr.ryan444@gmail.com>
2021-12-06 15:58:44 -0700

    Add ingredient tests to file

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -13,4 +13,28 @@ describe('Recipe', () => {
     cheese = new Ingredient(ingredientsData[1].id, ingredientsData[1].name, ingredientsData[1].estimatedCostInCents);
     butter = new Ingredient(ingredientsData[2].id, ingredientsData[2].name, ingredientsData[2].estimatedCostInCents);
   });
+
+  it('should be a function', () => {
+    expect(Ingredient).to.be.a('function');
+  });
+
+  it('should be an instance of Ingredient', () => {
+    expect(bread).to.be.an.instanceOf(Ingredient);
+  });
+
+  it('should have an id', () => {
+    expect(butter.id).to.eqaul(3);
+  });
+
+  it('should have a name', () => {
+    expect(cheese.name).to.equal('cheese');
+  });
+
+  it('should have an estimated cost in cents', () => {
+    expect(bread.estimatedCostInCents).to.equal(10);
+  });
+
+  it('should be able to calculate total cost', () => {
+    expect(bread.calculateCost(3)).to.equal(30);
+  });
 });
