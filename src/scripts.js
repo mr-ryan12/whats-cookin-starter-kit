69ac80902c9beb4cbac08ab587a23166c04337b1
Ryan McBride <mr.ryan444@gmail.com>
2021-12-08 18:22:50 -0700

    Add dropdown list for tags

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,8 @@
 import './styles.css';
 import apiCalls from './apiCalls';
+import Ingredient from './classes/Ingredient';
+import Recipe from './classes/Recipe';
+import RecipeRepository from './classes/RecipeRepository';
+import User from './classes/User';
+import recipes from './data/recipes';
 
-
-
-
-console.log('Hello world');
